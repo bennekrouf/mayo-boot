e702f6fd12d2bea54f41808b26c4dddf29dd149d
Mohamed <mohamed.bennekrouf@gmail.com>
2023-10-20 14:45:41 +0200

    Add log on term

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
+const { execSync } = require('child_process');
 const os = require('os');
 
 const environment = process.argv[2] || 'local';
@@ -10,22 +10,9 @@ const envFileName = `.env.${environment}`;
 console.log(`Starting with environment file: ${envFileName}`);
 
 const platform = platformArg || (os.platform() === 'darwin' ? 'ios' : 'android');
-const platformCommand = `npx react-native run-${platform}`;
-const envVar = `ENVFILE=${envFileName}`;
+const platformCommand = `ENVFILE=${envFileName} npx react-native run-${platform}`;
 
 console.log(`Attempting to start the app on ${platform}...`);
 
-const appProcess = spawn(platformCommand, [], {
-    stdio: 'inherit', // This will make the child's stdio the same as the parent's.
-    shell: true, 
-    env: {
-        ...process.env,
-        ENVFILE: envFileName
-    }
-});
-
-appProcess.on('exit', (code) => {
-    if (code !== 0) {
-        console.error(`Error starting app with exit code ${code}`);
-    }
-});
+// This will directly execute the command and keep it running in the terminal.
+execSync(platformCommand, { stdio: 'inherit' });
