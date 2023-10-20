8c2acb6a35f0ad4fa6209a222d5066ede8d8d1d3
Mohamed <mohamed.bennekrouf@gmail.com>
2023-10-20 14:19:19 +0200

    Add logs

--- cli.js
+++ cli.js
@@ -6,7 +6,11 @@ const os = require('os');
 const environment = process.argv[2] || 'local';
 const envFileName = `.env.${environment}`;
 
+console.log(`Starting with environment file: ${envFileName}`);
+
 const startCommand = `ENVFILE=${envFileName} yarn start`;
+
+console.log('Attempting to start Metro bundler...');
 
 exec(startCommand, (error) => {
     if (error) {
@@ -17,6 +21,8 @@ exec(startCommand, (error) => {
     console.log('Metro bundler started!');
 
     const platformCommand = os.platform() === 'darwin' ? `ENVFILE=${envFileName} yarn ios` : `ENVFILE=${envFileName} yarn android`;
+
+    console.log(`Attempting to start the app on ${os.platform() === 'darwin' ? 'iOS' : 'Android'}...`);
 
     exec(platformCommand, (error) => {
         if (error) {
