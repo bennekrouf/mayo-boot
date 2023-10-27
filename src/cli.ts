2bc73865c927927f44430169227fb30e8011f2df
Mohamed <mohamed.bennekrouf@gmail.com>
2023-10-27 15:57:01 +0200

    An android working version

--- src/cli.ts
+++ src/cli.ts
@@ -3,7 +3,7 @@ import fs from 'fs';
 import { execSync } from 'child_process';
 import os from 'os';
 import path from 'path';
-import net from 'net'; // Point 2: ES6 import for net
+import net from 'net';
 
 import { openTerminalWithCommand } from './openTerminalWithCommand';
 import { cleanAndroidBuildArtifacts, startAndroidApp } from './platforms/android';
@@ -14,41 +14,10 @@ const environment: string = process.argv[2] || 'local';
 const platformArg: string | null = process.argv[3] || null;
 const envFileName: string = `.env.${environment}`;
 
-const getLaunchPackagerPath = (): string => path.join(process.cwd(), 'node_modules', 'react-native', 'scripts', 'launchPackager.command');
+const getLaunchPackagerPath = (): string => 
+    path.join(process.cwd(), 'node_modules', 'react-native', 'scripts', 'launchPackager.command');
+
 const sleep = (ms: number) => new Promise(resolve => setTimeout(resolve, ms));
-
-const operationWithLaunchPackager = async (): Promise<void> => {
-    killAllMetroInstances();
-    
-    if (os.platform() === 'linux') {
-        // Use the standard command for Linux
-        execSync('npx react-native start', { stdio: 'inherit' });
-        await sleep(5000);  // Wait for 5 seconds to ensure Metro Bundler starts.
-        return;
-    }
-    
-    const launchPackagerPath: string = getLaunchPackagerPath();
-    if (await isPortInUse(8081)) {
-        console.log('Metro Bundler is already running on port 8081. Skipping...');
-        return;
-    }
-    
-    console.log(`Starting Metro Bundler with: ${launchPackagerPath}`);
-    openTerminalWithCommand(launchPackagerPath);
-    await sleep(5000);  // Wait for 5 seconds to ensure Metro Bundler starts.
-};
-
-const startApp = (platform: string, envFileName: string): void => {
-    const platformCommand: string = `ENVFILE=${envFileName} npx react-native run-${platform}`;
-    console.log(`Attempting to start the app on ${platform}...`);
-    execSync(platformCommand, { stdio: 'inherit' });
-};
-
-const getEntryPoint = (): string => {
-    const possibleEntryPoints: string[] = ['index.js', 'index.ts', 'index.tsx'];
-    return possibleEntryPoints.find(entry => fs.existsSync(path.join(process.cwd(), entry))) || 
-           (() => { throw new Error('No valid entry point (index.js, index.ts, or index.tsx) was found.') })();
-};
 
 const isPortInUse = (port: number): Promise<boolean> => new Promise(resolve => {
     const server: net.Server = net.createServer();
@@ -60,14 +29,51 @@ const isPortInUse = (port: number): Promise<boolean> => new Promise(resolve => {
     server.listen(port);
 });
 
+const startMetroBundler = async (): Promise<void> => {
+    killAllMetroInstances();
+    
+    if (os.platform() === 'linux') {
+        execSync('npx react-native start', { stdio: 'inherit' });
+        await sleep(5000);
+        return;
+    }
+
+    const launchPackagerPath: string = getLaunchPackagerPath();
+    if (await isPortInUse(8081)) {
+        console.log('Metro Bundler is already running on port 8081. Skipping...');
+        return;
+    }
+
+    console.log(`Starting Metro Bundler with: ${launchPackagerPath}`);
+    openTerminalWithCommand(launchPackagerPath);
+    await sleep(5000);
+};
+
+const startIOSApp = (envFileName: string): void => {
+    const platformCommand: string = `ENVFILE=${envFileName} npx react-native run-ios`;
+    console.log('Attempting to start the app on iOS...');
+    execSync(platformCommand, { stdio: 'inherit' });
+};
+
+const getEntryPoint = (): string => {
+    const possibleEntryPoints: string[] = ['index.js', 'index.ts', 'index.tsx'];
+    return possibleEntryPoints.find(entry => fs.existsSync(path.join(process.cwd(), entry))) || 
+           (() => { throw new Error('No valid entry point (index.js, index.ts, or index.tsx) was found.') })();
+};
+
 (async () => {
     console.log(`Starting with environment file: ${envFileName}`);
-    
     const platform: string = platformArg || (os.platform() === 'darwin' ? 'ios' : 'android');
     
-    await operationWithLaunchPackager(); 
     
-    platform === 'android' 
-        ? (cleanAndroidBuildArtifacts(process.cwd()), startAndroidApp(process.cwd(), envFileName))
-        : (cleanXcodeDerivedData(), cleanWatchmanCache(), bundleForiOS(getEntryPoint()), startApp(platform, envFileName));
+    if (platform === 'android') {
+        cleanAndroidBuildArtifacts(process.cwd());
+        startAndroidApp(process.cwd(), envFileName);
+    } else {
+        cleanXcodeDerivedData();
+        cleanWatchmanCache();
+        bundleForiOS(getEntryPoint());
+        startIOSApp(envFileName);
+    }
+    await startMetroBundler();
 })();
--- src/killAllMetroInstances.ts
+++ src/killAllMetroInstances.ts
@@ -2,10 +2,19 @@ import { execSync } from 'child_process';
 
 export const killAllMetroInstances = () => {
     try {
+        killProcessOnPort(8081);
         // Kill any process that has 'metro' in its description
         execSync('pkill -f metro');
         console.log("Killed all running Metro instances");
     } catch (error) {
         console.log("No running Metro instances found or an error occurred while trying to kill Metro processes");
+    }
+};
+
+const killProcessOnPort = (port: number): void => {
+    try {
+        execSync(`fuser -k ${port}/tcp`);
+    } catch (e) {
+        console.log(`No process to kill on port ${port} or an error occurred.`);
     }
 };
